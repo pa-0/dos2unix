946d4bd96e960a447a3e0e85e6be8c5e2ac91533
Erwin Waterlander <waterlan@xs4all.nl>
2011-06-28 06:43:03 +0000

    update German transaltion

--- dos2unix/common.h
+++ dos2unix/common.h
@@ -24,6 +24,18 @@
  *   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/* define feature test macros for realpath() -- needed on    */
+/* systems that have S_ISLNK, but chicken/egg means we must  */
+/* define early, before including stdlib.h (or sys/stat.h)   */
+#define _XOPEN_SOURCE 500
+
+/* similarly, instead of realpath we like to use, if         */
+/* available, the canonicalize_file_name() function, which   */
+/* is a GNU extension. We only ACTUALLY use the function if  */
+/* USE_CANONICALIZE_FILE_NAME is defined, but we don't define*/
+/* that until later. So...define the feature test macro now. */
+#define _GNU_SOURCE
+
 #ifdef ENABLE_NLS
 
 #include <libintl.h>
@@ -39,18 +51,6 @@
 #define bindtextdomain(Package, Directory)
 
 #endif
-
-/* define feature test macros for realpath() -- needed on    */
-/* systems that have S_ISLNK, but chicken/egg means we must  */
-/* define early, before including stdlib.h (or sys/stat.h)   */
-#define _XOPEN_SOURCE 500
-
-/* similarly, instead of realpath we like to use, if         */
-/* available, the canonicalize_file_name() function, which   */
-/* is a GNU extension. We only ACTUALLY use the function if  */
-/* USE_CANONICALIZE_FILE_NAME is defined, but we don't define*/
-/* that until later. So...define the feature test macro now. */
-#define _GNU_SOURCE
 
 #if defined(DJGPP) || defined(__TURBOC__) /* DJGPP */
 #  include <dir.h>
