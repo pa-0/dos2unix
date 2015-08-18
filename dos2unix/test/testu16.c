1a1e62779accc03048bd8fc656d36c576f22cc65
Erwin Waterlander <waterlan@xs4all.nl>
2015-08-18 17:27:01 +0000

    update comment in testu16.c

--- dos2unix/test/testu16.c
+++ dos2unix/test/testu16.c
@@ -13,8 +13,7 @@ int main () {
 
 /*
   When the output of this program is redirected to a file in Windows Command
-  Prompt you get narrow line breaks (0d0a) in it. It should be 0d00 0a00
-  for a line break.
+  Prompt you get for line breaks 0d0a 00, while it should be 0d00 0a00.
 
 c:\test>.\testu16.exe > o.txt
 
@@ -22,8 +21,11 @@ c:\test>xxd o.txt
 0000000: 6f00 6e00 6500 0d0a 0074 0077 006f 000d  o.n.e....t.w.o..
 0000010: 0a00 7400 6800 7200 6500 6500 0d0a 00    ..t.h.r.e.e....
 
+
+
   When the output is redirected to a file in PowerShell, null characters
-  0000 are inserted.
+  0000 are inserted. It looks like UTF-32 with an UTF-16 BOM and UTF-16 line
+  breaks.
 
 PS C:\test> .\testu16.exe > p.txt
 PS C:\test> xxd p.txt
