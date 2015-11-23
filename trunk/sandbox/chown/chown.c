e12d699ecf5fd85e4721837ced264fd47676c3cf
Erwin Waterlander <waterlan@xs4all.nl>
2015-11-23 20:10:43 +0000

    Tagging dos2unix release 7.3.2.

---
+++ trunk/sandbox/chown/chown.c
@@ -0,0 +1,68 @@
+int SetPrivilege(char *privilege, int enable)
+{
+    TOKEN_PRIVILEGES tp;
+    LUID luid;
+    HANDLE token;
+
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &token)) return 0;
+    if (!LookupPrivilegeValue(NULL, privilege, &luid)) return 0;
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    if (enable) tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    else tp.Privileges[0].Attributes = 0;
+
+    // Enable the privilege or disable all privileges.
+    return AdjustTokenPrivileges(token, 0, &tp, NULL, NULL, NULL);
+}
+
+
+    HANDLE token;
+    char *filename = "somefile.txt";
+    char *newuser = "someuser";
+    DWORD len;
+    PSECURITY_DESCRIPTOR security = NULL;
+    PSID sidPtr = NULL;
+    int retValue = 1;
+
+    // Get the privileges you need
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &token)) {
+        SetPrivilege(token, "SeTakeOwnershipPrivilege", 1);
+        SetPrivilege(token, "SeSecurityPrivilege", 1);
+        SetPrivilege(token, "SeBackupPrivilege", 1);
+        SetPrivilege(token, "SeRestorePrivilege", 1);
+    } else retValue = 0;
+
+    // Create the security descriptor
+    if (retValue) {
+        GetFileSecurity(filename, OWNER_SECURITY_INFORMATION, security, 0, &len);
+        security = (PSECURITY_DESCRIPTOR)malloc(len);
+        if (!InitializeSecurityDescriptor(security,
+SECURITY_DESCRIPTOR_REVISION))
+            retValue = 0;
+    }
+
+    // Get the sid for the username
+    if (retValue) {
+        char domainbuf[4096];
+        DWORD sidSize = 0;
+        DWORD bufSize = 4096;
+        SID_NAME_USE sidUse;
+        LookupAccountName(NULL, newuser, sidPtr, &sidSize, domainbuf, &bufSize, &sidUse);
+        sid = (PSID)malloc(sidSize);
+        if (!LookupAccountName(NULL, string, (PSID)sid, &sidSize, domainbuf, &bufSize, &sidUse))
+            retValue = 0;
+        }
+    }
+
+    // Set the sid to be the new owner
+    if (retValue && !SetSecurityDescriptorOwner(security, sidPtr, 0))
+        retValue = 0;
+
+    // Save the security descriptor
+    if (retValue)
+        retValue = SetFileSecurity(filename, OWNER_SECURITY_INFORMATION, security);
+    if (security) free(security);
+    if (sid) free(sid);
+    return retValue;
+
